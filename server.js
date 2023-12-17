5211c1acc972f136340355eb26c8c3e6fb46a512
kunalBando <144883534+kunalBando@users.noreply.github.com>
2023-12-17 16:10:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://nice-sky-08aa0150f.4.azurestaticapps.net', 'http://localhost:5173');
+  res.header('Access-Control-Allow-Origin', 'https://nice-sky-08aa0150f.4.azurestaticapps.net', 'https://victorious-ocean-0f5d4ab10.4.azurestaticapps.net', 'http://localhost:5173');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
